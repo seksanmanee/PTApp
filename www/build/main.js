67d061571b032e98c918fc19989d3529b6449439
seksanmanee <seksanmanee@gmail.com>
2018-11-14 08:10:45 +0700

    v2

--- www/build/main.js
+++ www/build/main.js
@@ -1582,7 +1582,7 @@ webpackContext.id = 508;
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(14);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__ = __webpack_require__(378);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(379);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_login_login__ = __webpack_require__(80);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_auth_auth__ = __webpack_require__(79);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__ngx_translate_core__ = __webpack_require__(30);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -1597,17 +1597,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
-//import {GoodReceiveDetailSavePage} from "../pages/good-receive-detail-save/good-receive-detail-save";
-//import {GoodReceiveDetailPage} from "../pages/good-receive-detail/good-receive-detail";
-//import {MenuPage} from "../pages/menu/menu";
-//import {GoodReceivePage} from "../pages/good-receive/good-receive";
 
 
 var MyApp = /** @class */ (function () {
     function MyApp(platform, statusBar, splashScreen, translate) {
         //rootPage: any = GoodReceiveDetailSavePage;
-        //rootPage: any = AuthPage;
-        this.rootPage = __WEBPACK_IMPORTED_MODULE_4__pages_login_login__["a" /* LoginPage */];
+        this.rootPage = __WEBPACK_IMPORTED_MODULE_4__pages_auth_auth__["a" /* AuthPage */];
+        //rootPage: any = LoginPage;
         this.defaultLang = 'en';
         // this language will be used as a fallback when a translation isn't found in the current language
         translate.setDefaultLang(this.defaultLang);
